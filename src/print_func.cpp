991fb1de7a85a5a4d7aca5c96485f75a9ba27986
m-anvl <mamaev-av18@ya.ru>
2026-03-25 01:31:41 +0400

    broke format else

--- src/print_func.cpp
+++ src/print_func.cpp
@@ -1,8 +1,13 @@
 #include "print_func.h"
 #include <cstdio>
 
+#define FOOO   0
+#define      BARRR   7
+#define    FOOBAR  4     
+
+
 void print_string(const char* str_ptr){
 printf("%s\n", str_ptr);  
-    return      ;      
+            return      ;      
 }
 
