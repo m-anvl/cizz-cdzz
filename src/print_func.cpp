3a4091d22eb8cb70b7816d4b2da48e0d0929df59
m-anvl <mamaev-av18@ya.ru>
2026-03-25 21:34:55 +0400

    push in master correct code

--- src/print_func.cpp
+++ src/print_func.cpp
@@ -2,11 +2,12 @@
 #include <cstdio>
 
 #define FOOO   0
-#define      BARRR   7
-#define         FOOBAR  4     
+#define BARRR  7
+#define FOOBAR 4
 
-
-
+static void print_foo() {
+    printf("foo");
+}
 
 
 void print_string(const char* str_ptr){
